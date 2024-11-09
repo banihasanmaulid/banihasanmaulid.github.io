f3e8ab19480c66835d4f9a7f7e72ba0574294050
banihasan <bani.maulid@indocyber.id>
2024-11-10 05:32:37 +0700

    sdasd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
    return (
       <div>
-         <BrowserRouter basename="/">
-            <BaseLayout/>
+         <BrowserRouter basename="/banihasanmaulid.github.io">
+            <BaseLayout />
          </BrowserRouter>
       </div>
    );
 }
-
 
 export default App;
